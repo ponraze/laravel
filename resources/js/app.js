75b1e21787ef1d042918d03ae25d5763d63e9c68
Vladimir <ponraze@gmail.com>
2023-12-27 18:51:15 +0300

    Add VueJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
 
-import Alpine from 'alpinejs';
+const app = createApp({});
 
-window.Alpine = Alpine;
-
-Alpine.start();
+app.mount("#app");
